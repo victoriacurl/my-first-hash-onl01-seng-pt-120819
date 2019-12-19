5c1301825cac2a38c2a08ebc15253c4615d6218c
Victoria Curl <victoriacurl9@gmail.com>
2019-12-19 21:48:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-end 
+end
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 
